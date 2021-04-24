7bc8ccbe6d5f121b2fc27899243d8e4d6834c9d7
Matthew Mason <mmason@medusamedical.com>
2021-04-24 14:59:18 -0300

    Adding Block types and updating tile structs Removing pull request build jobs, just push now

---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Assets.Scripts.Enums;
+
+public struct GameTile
+{
+    public BlockType blockType;
+    public int variant;
+    public float cost;
+}
