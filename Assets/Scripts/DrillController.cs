8171c3aa442b95a38c02e9b4aebdfc8280ab8a83
Matthew Mason <mmason@medusamedical.com>
2021-04-24 22:21:20 -0300

    Adding movement, fog of war, clearing fog, prefabs

---
+++ Assets/Scripts/DrillController.cs
@@ -0,0 +1,91 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class DrillController : MonoBehaviour
+{
+    public Vector2Int position;
+    public float visionRadius = 3.25f;
+
+    private Vector2Int Position
+    {
+        get => position;
+    }
+
+    private Vector2Int facingDirection;
+
+    public Vector2 FacingDirection
+    {
+        get => facingDirection;
+    }
+
+    private GameManager gameManager;
+    private LevelManager levelManager;
+
+    public float timePerAction = 1.0f;
+    private float timePassed = 0.0f;
+
+    public void Awake()
+    {
+        facingDirection = Vector2Int.down;
+    }
+
+    public void Start()
+    {
+        gameManager = FindObjectOfType<GameManager>();
+        levelManager = FindObjectOfType<LevelManager>();
+    }
+
+    public void FixedUpdate()
+    {
+        timePassed += Time.fixedDeltaTime;
+
+        if (timePassed > timePerAction)
+        {
+            timePassed -= timePerAction;
+            MoveCharacter();
+        }
+    }
+
+    private void MoveCharacter()
+    {
+        if (facingDirection != Vector2.zero)
+        {
+            position += facingDirection;
+        }
+
+        this.transform.position = new Vector3(position.x, position.y, 0.0f);
+        levelManager.ClearFogOfWar(position.x, position.y, visionRadius);
+    }
+
+    private void OnMove(InputValue input)
+    {
+        Vector2 vec = input.Get<Vector2>();
+        Vector2Int moveVec = Vector2Int.zero;
+
+        if (vec.x != 0.0f)
+        {
+            moveVec.x = (vec.x < 0) ? -1 : 1;
+        }
+
+        if (vec.y != 0.0f)
+        {
+            moveVec.y = (vec.y < 0) ? -1 : 1;
+        }
+
+        if (moveVec.x != 0 && moveVec.y != 0)
+        {
+            moveVec.x = 0;
+        }
+
+        if (moveVec != Vector2.zero)
+        {
+            if (moveVec != (facingDirection * -1))
+            {
+                facingDirection = moveVec;
+            }
+        }
+    }
+}
