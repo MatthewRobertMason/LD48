c5a579ed9ded480e7a6d26d51b712372fa32bbe1
Matthew Mason <mmason@medusamedical.com>
2021-04-26 18:49:27 -0300

    Audio Marquee

---
+++ Assets/Scripts/TextMarquee.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextMarquee : MonoBehaviour
+{
+    public RectTransform MaskContainer;
+    public Text text;
+
+    public float speed;
+    public float percentDistance = 0.0f;
+    public float distanceRequired;
+
+    private AudioManager audioManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioManager = FindObjectOfType<AudioManager>();
+        audioManager.SongName = this;
+        audioManager.SetSongName(audioManager.SourceAudio.clip);
+        SetUpText(text.text);
+    }
+
+    public void SetUpText(string value)
+    {
+        text.text = value;
+
+        float x = MaskContainer.rect.width;
+        float y = text.rectTransform.position.y;
+        float z = text.rectTransform.position.z;
+
+        text.rectTransform.position = new Vector3(x, y, 0.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveDist = speed * Time.deltaTime;
+        
+        float x = text.rectTransform.anchoredPosition.x - moveDist;
+        float y = text.rectTransform.anchoredPosition.y;
+        
+        if (text.rectTransform.anchoredPosition.x <= -text.preferredWidth)
+        {
+            x = MaskContainer.rect.width;
+        }
+
+        text.rectTransform.anchoredPosition = new Vector2(x, y);
+    }
+}
